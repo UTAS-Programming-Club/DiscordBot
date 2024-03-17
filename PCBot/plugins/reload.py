271041382d5f092522d996f50193fb6c6430349e
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-03-18 01:15:41 +1100

    Fix reload command failing to load unloaded plugins
    Made sure loaded plugins are reloaded.
    Restructed reload.py to follow PEP8(general style) and PEP257(docstring style).
    Log which plugins are loaded after reloading.

--- PCBot/plugins/reload.py
+++ PCBot/plugins/reload.py
@@ -1,32 +1,53 @@
 import crescent
 import hikari
 import logging
+import sys
 from crescent.ext import docstrings
 
-# TODO: Prevent error when loading unloaded plugin, issue with refresh?
+# TODO: Fix newly loaded plugins not showing up in discord
+# TODO: Restrict command to committee members
+# TODO: Indicate which commands are from each plugin
+# TODO: Indicate which plugins/commands are new, modified or unmodified
 
 # Load guild id
 with open('./secrets/guild') as f:
-  guildId = int(f.read().strip())
+    guild_id = int(f.read().strip())
 
+logger = logging.getLogger(__name__)
 plugin = crescent.Plugin[hikari.GatewayBot, None]()
+
 
 @plugin.include
 @docstrings.parse_doc
-@crescent.command(guild = guildId)
+@crescent.command(guild=guild_id)
 async def reload(ctx: crescent.Context) -> None:
-  """
-  Reload the bot.
-  
-  Requested by something sensible(somethingsensible).
-  Implemented by something sensible(somethingsensible).
-  """
-  await ctx.respond('Reloading...', ephemeral = True)
-  plugins = plugin.client.plugins
-  # TODO: Fix this causing errors or not actually loading any plugin changes
-  # The current issue is that this copy of load_folder will error if an unloaded
-  # plugin is detected either because it is new or it errored on a previous load
-  #plugins.unload_all()
-  plugins.load_folder('PCBot.plugins', refresh = True)
-  await ctx.edit('Reloaded')
-  logging.getLogger(__name__).info("Reloaded")
+    """
+    Reload the bot.
+
+    Requested by something sensible(somethingsensible).
+    Implemented by something sensible(somethingsensible).
+    """
+    await ctx.respond('Reloading...', ephemeral=True)
+
+    # Used to avoid an error with plugin being undefined after unload_all
+    plugins = plugin.client.plugins
+    # Used to avoid the first load_folder erroring because it tried to load an
+    # already loaded plugin
+    plugins.unload_all()
+    # Load unloaded plugins, if any plugin it finds was previously loaded then
+    # the old code is reused
+    plugins.load_folder('PCBot.plugins')
+    # Reload previously loaded plugins to use newest code
+    new_plugins = plugins.load_folder('PCBot.plugins', refresh=True)
+
+    logger.info("Reloaded")
+    await ctx.edit('Reloaded')
+
+    loaded_plugin_names = tuple([
+        module_name.split('.')[-1]
+        for module_name, module in sys.modules.items()
+        if 'PCBot.plugins.' in module_name
+        if getattr(module, "plugin", None) in new_plugins
+    ])
+    logger.info(f"Loaded plugins: {', '.join(loaded_plugin_names)}")
+    await ctx.respond(f"Loaded plugins: {', '.join(loaded_plugin_names)}")
--- README.md
+++ README.md
@@ -19,7 +19,9 @@ python -OO -m PCBot
 # Desired Features
 PC club members are welcome to contribute and there is a section in each docstring for credits
 * Type checking
-* Restricting reload command to committee members
+* PEP8 AND PEP257 checking, https://www.codewof.co.nz/style/python3/ works until that happens though
+* Show full command docstrings somewhere, help command? Then have non pinging mentions for required and implemented by lines
+* Make guild id accessable to all plugins, I don't think BotData cannot be used since the plugin is unloaded when it is needed
 * More commands
 * Anything listed as TODO within the plugins
 
