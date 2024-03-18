8299e509f9823049eb4a549fb1cea6c9965923eb
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-03-18 13:11:22 +1100

    Restructure rockpaperscissors.py to follow PEP8 and PEP257
    The exception is the RPSPick enum which I think is more readable as is.

--- PCBot/plugins/rockpaperscissors.py
+++ PCBot/plugins/rockpaperscissors.py
@@ -1,3 +1,5 @@
+"""This module contains the bot's rock paper scissors minigame command."""
+
 import crescent
 import hikari
 import miru
@@ -8,141 +10,176 @@ from typing import Optional
 
 # Load guild id
 with open('./secrets/guild') as f:
-  guildId = int(f.read().strip())
+    guild_id = int(f.read().strip())
 
 plugin = crescent.Plugin[hikari.GatewayBot, BotData]()
 
 # Not useful for this as command options can be seen by users.
 # Keeping as autocomplete may be useful for something else.
-#async def rps_autocomplete_response(
-#  ctx: crescent.AutocompleteContext, option: hikari.AutocompleteInteractionOption
-#) -> list[tuple[str, str]]:
-#  return
+# async def rps_autocomplete_response(
+#     ctx: crescent.AutocompleteContext,
+#     option: hikari.AutocompleteInteractionOption
+# ) -> list[tuple[str, str]]:
+#     return
 #
-#async def fetch_rps_autocomplete_options(
-#  ctx: crescent.AutocompleteContext, option: hikari.AutocompleteInteractionOption
-#) -> list[tuple[str, str]]:
-#    _options = await ctx.fetch_options()
-#    return []
+#
+# async def fetch_rps_autocomplete_options(
+#   ctx: crescent.AutocompleteContext,
+#   option: hikari.AutocompleteInteractionOption
+# ) -> list[tuple[str, str]]:
+#     _options = await ctx.fetch_options()
+#     return []
 
 # Not useful as returning False from modal_check still closes the Modal.
 # I was hoping that it would show an error and allow changing input.
 # Keeping as it may be useful for something else.
-#class RPSModal(miru.Modal, title = 'Rock Paper Scissors'):
-#  pick = miru.TextInput(
-#    label = 'Rock, Paper or Scissors',
-#    required = True,
-#    min_length = 4,
-#    max_length = 8,
-#  )
+# class RPSModal(miru.Modal, title='Rock Paper Scissors'):
+#     pick = miru.TextInput(
+#       label='Rock, Paper or Scissors',
+#       required=True,
+#       min_length=4,
+#       max_length=8,
+#     )
 #
-#  async def modal_check(self, ctx: miru.ModalContext) -> bool:
-#    if ctx.values[self.pick].casefold() in ['rock', 'paper', 'scissors']:
-#      return True
-#    else:
-#      await ctx.respond(f'You entered "{ctx.values[self.pick]}" which is not allowed')
-#      return False
+#     async def modal_check(self, ctx: miru.ModalContext) -> bool:
+#         if ctx.values[self.pick].casefold() in ['rock', 'paper', 'scissors']:
+#             return True
+#         else:
+#             await ctx.respond(
+#               f'You entered "{ctx.values[self.pick]}" which is not allowed'
+#             )
+#             return False
 #
-#  # The callback function is called after the user hits 'Submit'
-#  async def callback(self, ctx: miru.ModalContext) -> None:
-#    # You can also access the values using ctx.values,
-#    # Modal.values, or use ctx.get_value_by_id()
-#    await ctx.respond(
-#      f'You picked {self.pick.value}'
-#    )
-#
+#     # The callback function is called after the user hits 'Submit'
+#     async def callback(self, ctx: miru.ModalContext) -> None:
+#         # You can also access the values using ctx.values,
+#         # Modal.values, or use ctx.get_value_by_id()
+#         await ctx.respond(
+#           f'You picked {self.pick.value}'
+#         )
 # Above is sent with:
 # modal = RPSModal()
 # builder = modal.build_response(plugin.model.miru)
 # await ctx.respond_with_builder(builder)
 # plugin.model.miru.start_modal(modal)
 
-class RPSPick(Enum):
-  Rock     = 1
-  Paper    = 2
-  Scissors = 3
-
+# TODO: Add a variant for more than 2 people
 # TODO: Prevent users changing their pick
 # TODO: Tell user when their pick has been received
 # TODO: Report timeout
 # TODO: Disable buttons visibly after the round is over or timeout has occurred
 # TODO: Convert to user command or at least offer that option
 
+
+class RPSPick(Enum):
+    """Enum to store each user's rock paper scissors selection."""
+
+    Rock     = 1
+    Paper    = 2
+    Scissors = 3
+
+
 class RPSView(miru.View):
-  challenger: hikari.User
-  challengee: hikari.User
-  challengerPick: Optional[RPSPick] = None
-  challengeePick: Optional[RPSPick] = None
+    """Miri view with buttons to obtain each user's selection."""
 
-  async def determine_winner(self, ctx: miru.ViewContext) -> None:
-    if self.challengerPick is None or self.challengeePick is None:
-      return
-    if self.challengerPick == self.challengeePick:
-      await ctx.edit_response(f'It was a tie!')
-    elif ((self.challengerPick is RPSPick.Paper and self.challengeePick is RPSPick.Rock) or
-       (self.challengerPick is RPSPick.Scissors and self.challengeePick is RPSPick.Paper) or
-       (self.challengerPick is RPSPick.Rock and self.challengeePick is RPSPick.Scissors)):
-      await ctx.edit_response(f'{self.challengerPick.name} beats {self.challengeePick.name.lower()}, {self.challenger.mention} wins!')
-    else:
-      await ctx.edit_response(f'{self.challengeePick.name} beats {self.challengerPick.name.lower()}, {self.challengee.mention} wins!')
-    self.stop()
+    challenger: hikari.User
+    challengee: hikari.User
+    challenger_pick: Optional[RPSPick] = None
+    challengee_pick: Optional[RPSPick] = None
 
-  @miru.button(label='Rock', emoji='\N{ROCK}', style=hikari.ButtonStyle.PRIMARY)
-  async def rock_button(self, ctx: miru.ViewContext, button: miru.Button) -> None:
-    if ctx.user.username == self.challenger.username:
-      self.challengerPick = RPSPick.Rock
-    else:
-      self.challengeePick = RPSPick.Rock
-    await self.determine_winner(ctx)
+    async def determine_winner(self, ctx: miru.ViewContext) -> None:
+        """Determine if the game has finished and if so who won."""
+        if self.challenger_pick is None or self.challengee_pick is None:
+            return
+        if self.challenger_pick == self.challengee_pick:
+            await ctx.edit_response('It was a tie!')
+        elif ((
+           self.challenger_pick is RPSPick.Paper and
+           self.challengee_pick is RPSPick.Rock
+          ) or (
+           self.challenger_pick is RPSPick.Scissors and
+           self.challengee_pick is RPSPick.Paper
+          ) or (
+           self.challenger_pick is RPSPick.Rock and
+           self.challengee_pick is RPSPick.Scissors
+        )):
+            await ctx.edit_response(
+              f'{self.challenger_pick.name} beats '
+              f'{self.challengee_pick.name.lower()}, '
+              f'{self.challenger.mention} wins!'
+            )
+        else:
+            await ctx.edit_response(
+              f'{self.challengee_pick.name} beats '
+              f'{self.challenger_pick.name.lower()}, '
+              f'{self.challengee.mention} wins!'
+            )
+        self.stop()
 
-  @miru.button(label='Paper', emoji='\N{SCROLL}', style=hikari.ButtonStyle.PRIMARY)
-  async def paper_button(self, ctx: miru.ViewContext, button: miru.Button) -> None:
-    if ctx.user.username == self.challenger.username:
-      self.challengerPick = RPSPick.Paper
-    else:
-      self.challengeePick = RPSPick.Paper
-    await self.determine_winner(ctx)
+    @miru.button(label='Rock', emoji='\N{ROCK}',
+                 style=hikari.ButtonStyle.PRIMARY)
+    async def rock_button(self, ctx: miru.ViewContext,
+                          button: miru.Button) -> None:
+        """Handle the rock button being clicked."""
+        if ctx.user.username == self.challenger.username:
+            self.challenger_pick = RPSPick.Rock
+        else:
+            self.challengee_pick = RPSPick.Rock
+        await self.determine_winner(ctx)
 
-  @miru.button(label='Scissors', emoji='\N{BLACK SCISSORS}', style=hikari.ButtonStyle.PRIMARY)
-  async def scissors_button(self, ctx: miru.ViewContext,  button: miru.Button) -> None:
-    if ctx.user.username == self.challenger.username:
-      self.challengerPick = RPSPick.Scissors
-    else:
-      self.challengeePick = RPSPick.Scissors
-    await self.determine_winner(ctx)
+    @miru.button(label='Paper', emoji='\N{SCROLL}',
+                 style=hikari.ButtonStyle.PRIMARY)
+    async def paper_button(self, ctx: miru.ViewContext,
+                           button: miru.Button) -> None:
+        """Handle the paper button being clicked."""
+        if ctx.user.username == self.challenger.username:
+            self.challenger_pick = RPSPick.Paper
+        else:
+            self.challengee_pick = RPSPick.Paper
+        await self.determine_winner(ctx)
 
-  async def view_check(self, ctx: miru.ViewContext) -> bool:
-    #await ctx.respond(f'{ctx.user.username} pressed a button')
-    #await ctx.respond(
-    #  f'Challenger: {self.challenger.username}\n'
-    #  f'Challengee: {self.challengee.username}\n'
-    #  f'BtnPresser: {ctx.user.username}'
-    #)
-    return ctx.user.username == self.challenger.username or ctx.user.username == self.challengee.username
+    @miru.button(label='Scissors', emoji='\N{BLACK SCISSORS}',
+                 style=hikari.ButtonStyle.PRIMARY)
+    async def scissors_button(self, ctx: miru.ViewContext,
+                              button: miru.Button) -> None:
+        """Handle the scissors button being clicked."""
+        if ctx.user.username == self.challenger.username:
+            self.challenger_pick = RPSPick.Scissors
+        else:
+            self.challengee_pick = RPSPick.Scissors
+        await self.determine_winner(ctx)
+
+    async def view_check(self, ctx: miru.ViewContext) -> bool:
+        """Before calling button methods check if presser is allowed user."""
+        return (ctx.user.username == self.challenger.username or
+                ctx.user.username == self.challengee.username)
+
 
 @plugin.include
 @docstrings.parse_doc
-@crescent.command(guild = guildId, dm_enabled = False)
-class rpschallenge:
-  """
-  Challenge a user to rock paper scissors.
-  
-  Requested by iluka wighton(razer304).
-  Implemented by something sensible(somethingsensible).
-  
-  Args:
-      user: User to challenge.
-  """
-  #pick: Your pick, this is not shown to the other user.
-  user = crescent.option(hikari.User)
-  
-  async def callback(self, ctx: crescent.Context) -> None:
-    view = RPSView()
-    view.challenger = ctx.user
-    view.challengee = self.user.user
-    await ctx.respond(
-      f'{self.user.user.mention} You have been challenged to Rock Paper Scissors!\n'
-       'Both players now need to make their decision below:',
-      components = view
-    )
-    plugin.model.miru.start_view(view)
+@crescent.command(name='rpschallenge', guild=guild_id, dm_enabled=False)
+class RPSChallengeCommand:
+    """
+    Challenge a user to rock paper scissors.
+
+    Requested by iluka wighton(razer304).
+    Implemented by something sensible(somethingsensible).
+
+    Args:
+        user: User to challenge.
+    """
+
+    user = crescent.option(hikari.User)
+
+    async def callback(self, ctx: crescent.Context) -> None:
+        """Handle rpschallenge command being run by showing button view."""
+        view = RPSView()
+        view.challenger = ctx.user
+        view.challengee = self.user.user
+        await ctx.respond(
+          f'{self.user.user.mention}'
+          'You have been challenged to Rock Paper Scissors!\n'
+          'Both players now need to make their decision below:',
+          components=view
+        )
+        plugin.model.miru.start_view(view)
