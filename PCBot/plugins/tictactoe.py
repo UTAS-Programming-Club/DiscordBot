61557aab73baacda7cec397de16663000bf36eb9
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-03-23 01:18:09 +1100

    Add experimental support for more tic tac toe players
    Only really make sense for 5x5 and perhaps 4x4.
    Fix non experimental mode erroring.

--- PCBot/plugins/tictactoe.py
+++ PCBot/plugins/tictactoe.py
@@ -5,21 +5,34 @@ import hikari
 import miru
 import re
 from crescent.ext import docstrings
+from enum import Enum
 from PCBot.botdata import BotData
 from typing import Optional
 
 # TODO: Disable buttons on timeout
 # TODO: Disable buttons when the bot shuts down, does reload also break views?
+# TODO: Add option to change starting player. Have random option?
 # TODO: Support larger grid sizes
 # TODO: Support more players?
 #      The win detector can support it, just not sure if it works gameplay wise
 
 plugin = crescent.Plugin[hikari.GatewayBot, BotData]()
 
-# Blue
-challenger_style = hikari.ButtonStyle.PRIMARY
-# Red
-challengee_style = hikari.ButtonStyle.DANGER
+
+class Player(Enum):
+    """Enum to store the different players."""
+
+    Challenger  = 0
+    Challengee1 = 1
+    Challengee2 = 2
+
+
+player_styles = [
+  hikari.ButtonStyle.PRIMARY,  # Blue
+  hikari.ButtonStyle.DANGER,   # Red
+  hikari.ButtonStyle.SUCCESS   # Green
+]
+
 # Gray
 default_style = hikari.ButtonStyle.SECONDARY
 
@@ -27,20 +40,20 @@ default_style = hikari.ButtonStyle.SECONDARY
 class TicTacToeView(miru.View):
     """Miri view with buttons to show the state and allow user selection."""
 
-    challenger: hikari.User
-    challengee: hikari.User
-    current_player: hikari.User
+    players: list[hikari.User]
+    current_player: Player
 
     grid_size: int
 
     def __init__(self, challenger: hikari.User, challengee: hikari.User,
-                 grid_size: int) -> None:
+                 challengee2: Optional[hikari.User], grid_size: int) -> None:
         """Create view and buttons to manage tic tac toe game."""
         super().__init__()
-        self.challenger = challenger
-        self.challengee = challengee
+        self.players = [challenger, challengee]
+        if challengee2 is not None:
+            self.players.append(challengee2)
         # This is arbitrary
-        self.current_player = self.challenger
+        self.current_player = Player.Challenger
         self.grid_size = grid_size
         for cell_value in range(grid_size * grid_size):
             self.add_item(TicTacToeButton(cell_value, grid_size))
@@ -57,12 +70,13 @@ class TicTacToeView(miru.View):
     def check_diagonals(self, board: list[list[hikari.ButtonStyle]])\
     -> Optional[hikari.ButtonStyle]:
         """Check if a player has board size pieces along a diagonal."""
-        if len(set([board[i][i] for i in range(len(board))])) == 1:
+        if (len(set([board[i][i] for i in range(len(board))])) == 1 and
+             board[0][0] != default_style):
             return board[0][0]
         if len(set([
                       board[i][len(board)-i-1]
                       for i in range(len(board))
-                   ])) == 1:
+                   ])) == 1 and board[0][len(board)-1] != default_style:
             return board[0][len(board)-1]
         return None
 
@@ -104,9 +118,12 @@ class TicTacToeView(miru.View):
         # Check for winner
         board = self.produce_board()
         winner_style = self.check_win(board)
+        # This should not happen but I want to see if it ever does
+        if winner_style == default_style:
+            print(winner_style)
+            print(board)
         draw = self.check_draw()
-        if (not draw and winner_style != challenger_style and
-           winner_style != challengee_style):
+        if not draw and winner_style not in player_styles:
             return False
 
         # Disable interaction
@@ -127,7 +144,7 @@ class TicTacToeView(miru.View):
 
     async def view_check(self, ctx: miru.ViewContext) -> bool:
         """Before calling button methods check if presser is current player."""
-        return ctx.user.username == self.current_player.username
+        return ctx.user.username == self.players[self.current_player.value].username
 
 
 class TicTacToeButton(miru.Button):
@@ -146,12 +163,10 @@ class TicTacToeButton(miru.Button):
     async def callback(self, ctx: miru.ViewContext) -> None:
         """Respond to button presses by restyling the button."""
         # Prepare changes to button
-        if ctx.user.username == self.view.challenger.username:
-            self.style = challenger_style
-            self.view.current_player = self.view.challengee
-        else:
-            self.style = challengee_style
-            self.view.current_player = self.view.challenger
+        self.style = player_styles[self.view.current_player.value]
+        new_player_index =\
+          (self.view.current_player.value + 1) % len(self.view.players)
+        self.view.current_player = Player(new_player_index)
         self.disabled = True
 
         # Check for winner, this must be after button changes so that the most
@@ -162,7 +177,7 @@ class TicTacToeButton(miru.Button):
 
         # Prepare modified message with new current player mention
         message = ctx.message.content
-        new_mention = self.view.current_player.mention + "'"
+        new_mention = self.view.players[new_player_index].mention + "'"
         updated_message = re.sub(r"<@\d+>'", new_mention, message, 1)
 
         # Update game message
@@ -188,26 +203,39 @@ class TicTacToeCommand:
         bool, 'Allow the use of experimental settings',
         default=False
     )
-    experimental_grid_size = crescent.option(int, default=3)
+    experimental_grid_size = crescent.option(int, default=3,
+                                             min_value=2, max_value=5)
+    experimental_third_player = crescent.option(hikari.User, default=None)
 
     async def callback(self, ctx: crescent.Context) -> None:
         """Handle tictactoe command being run by showing button grid view."""
+        mention_line = self.user.user.mention
+        title_prefix = ''
+        colour_line_suffix = ''
         if self.allow_experimental:
+            if self.experimental_third_player is not None:
+                challengee2 = self.experimental_third_player.user
+                mention_line += f' {challengee2.mention}'
+                colour_line_suffix += f', green is {challengee2.mention}'
+            else:
+                challengee2 = None
             view = TicTacToeView(
                 challenger=ctx.user, challengee=self.user.user,
-                grid_size=self.experimental_grid_size
+                challengee2=challengee2, grid_size=self.experimental_grid_size
             )
-            title_prefix = 'experimental'
+            title_prefix += 'experimental'
         else:
             view = TicTacToeView(
-                challenger=ctx.user, challengee=self.user.user
+                challenger=ctx.user, challengee=self.user.user,
+                challengee2=None, grid_size=3
             )
-            title_prefix = ''
         await ctx.respond(
-          f'{self.user.user.mention} '
-          f'You have been challenged to {title_prefix} Tic Tac Toe!\n'
-          f'Blue is {ctx.user.mention}, red is {self.user.user.mention}.\n'
-          f"It is currently {view.current_player.mention}'s turn.\n",
+          mention_line +
+          f' You have been challenged to {title_prefix} Tic Tac Toe!\n'
+          f'Blue is {ctx.user.mention}, red is {self.user.user.mention}'
+          f'{colour_line_suffix}.\n'
+          'It is currently '
+          f"{view.players[view.current_player.value].mention}'s turn.\n",
           components=view
         )
         plugin.model.miru.start_view(view)
