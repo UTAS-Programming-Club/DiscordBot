009e31e2cc7aa2cd10f5e45d7301b44c6a19bee9
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2024-05-02 12:02:09 +1000

    Allow input to text adventure using replies

--- PCBot/plugins/textadventure.py
+++ PCBot/plugins/textadventure.py
@@ -5,11 +5,25 @@ import hikari
 import os.path
 from cffi import FFI
 from crescent.ext import docstrings
+from enum import Enum
+from typing import Any
 
 if not os.path.isfile('GameData.json') or not os.path.isfile('game.so'):
     raise Exception('Unable to find required game files')
 
 plugin = crescent.Plugin[hikari.GatewayBot, None]()
+# TODO: Figure out type for cffi's "struct GameOutput *" and use instead of Any
+games: dict[hikari.snowflakes.Snowflake, Any] = {}
+
+
+class InputOutcome(Enum):
+    """Represents the possible outcomes for different inputs."""
+
+    InvalidInputOutcome = 0
+    GetNextOutput = 1
+    QuitGame = 2
+    GotoScreen = 3
+
 
 # TODO: Switch to other method so that including headers is allowed
 ffi = FFI()
@@ -61,8 +75,76 @@ ffi.cdef("""
 
     bool SetupGame(void);
     bool GetCurrentGameOutput(struct GameOutput *);
+    enum InputOutcome HandleGameInput(enum ScreenID, uint32_t);
+
+// From screens.h
+    void FreeScreen(struct GameOutput *);
 """)
 game = ffi.dlopen("./game.so")
+
+
+async def handle_output(message: hikari.Message):
+    """Get and respond with the current game state."""
+    output = games[message.id]
+    succeeded: bool = game.GetCurrentGameOutput(output)
+    if (not succeeded):
+        await message.edit("Unable to get game output")
+        return
+
+    response = ('```' + ffi.string(output.body) + '```'
+                + '\nReply to this message with one of the numbers below to '
+                + 'choose that option:\n')
+    for i in range(output.inputCount):
+        input_str = ffi.string(output.inputs[i].title)
+        response += f'{i + 1}. {input_str}\n'
+
+    await message.edit(response)
+
+
+async def handle_input(message: hikari.Message, input_index: int):
+    """Update the current game state given the last input."""
+    output = games[message.id]
+
+    outcome_value = game.HandleGameInput(output.screenID, input_index)
+    outcome = InputOutcome(outcome_value)
+
+    match outcome:
+        case InputOutcome.GetNextOutput:
+            game.FreeScreen(output)
+            await handle_output(message)
+        case InputOutcome.QuitGame:
+            game.FreeScreen(output)
+            await message.edit('The game is over!')
+
+
+@plugin.include
+@crescent.event
+async def on_game_message_create(event: hikari.MessageCreateEvent):
+    """Handle users replying to game messages."""
+    if event.message.referenced_message is None:
+        return
+    game_message = event.message.referenced_message
+
+    if game_message.author == hikari.undefined.UNDEFINED:
+        return
+
+    if game_message.author.id != plugin.app.get_me().id:
+        return
+
+    if game_message.id not in games:
+        return
+    game_output = games[game_message.id]
+
+    user_input = event.message.content.replace(' ', '')
+    if not user_input.isdigit():
+        return
+
+    input_index = int(user_input) - 1
+    if input_index >= 255:  # Input index is a uint8_t in c
+        return
+
+    await event.message.delete()
+    await handle_input(game_message, input_index)
 
 
 @plugin.include
@@ -76,26 +158,12 @@ class TextCommand:
     Implemented by something sensible(somethingsensible).
     """
 
-    async def handle_output(self, ctx: crescent.Context, output):
-        """Get and respond with the current game state."""
-        succeeded: bool = game.GetCurrentGameOutput(output)
-        if (not succeeded):
-            await ctx.respond("Unable to get game output")
-            return
-
-        response = ('```' + ffi.string(output.body) + '```'
-                    + '\nIn future the following options will be available:\n')
-        for i in range(output.inputCount):
-            input_str = ffi.string(output.inputs[i].title)
-            response += f'{i + 1}. {input_str}\n'
-
-        await ctx.respond(response)
-
     async def callback(self, ctx: crescent.Context) -> None:
         """Handle text command being run."""
         if (not game.SetupGame()):
             await ctx.respond("Unable to start game")
             return
 
-        output = ffi.new("struct GameOutput *")
-        await self.handle_output(ctx, output)
+        message = await ctx.respond('Setting up game', ensure_message=True)
+        games[message.id] = ffi.new("struct GameOutput *")
+        await handle_output(message)
