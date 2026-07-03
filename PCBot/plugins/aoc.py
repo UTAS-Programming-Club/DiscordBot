1cc84cf07f181a2ca5923b0ca0a1ac785c60cd53
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2026-07-03 22:32:55 +1000

    Disable aoc command

--- PCBot/plugins/aoc.py
+++ PCBot/plugins/aoc.py
@@ -111,6 +111,7 @@ async def autocomplete_score_type(
     return [(st.name, st.name) for st in ScoreType]
 
 
+```
 @plugin.include
 @tasks.loop(hours=0, minutes=30, seconds=0)
 async def update_leaderboard() -> None:
@@ -287,3 +288,4 @@ class AOCCommand:
         await ctx.respond(output, embed=embed, user_mentions=False)
 
         await update_names()
+```
